dd1a9c44bac010ae2176a03b9245177016de2618
Souptik Sarkar <souptiksarkar4572@gmail.com>
2022-04-05 06:22:18 +0530

    feat: add cors support in our app

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import { configureMongoose } from './config/mongooseConfig.js';
 import pitchRouter from './routes/pitch.js';
+import cors from 'cors';
 
 const app = express();
+
+// Enable cors support in our application
+app.use(cors())
 
 // Attaching json middlewares for better json parsing
 app.use(express.json());
@@ -19,3 +23,14 @@ const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
 	console.log(`Server is up and running at http://localhost:${PORT}`);
 });
+
+
+// https://rebrand.ly/xharktank-pitches-https/?author=souptiksarkar4572&url=https://xharktank.herokuapp.com
+
+// https://rebrand.ly/xharktank-invest-https/?author=souptiksarkar4572&url=https://xharktank.herokuapp.com
+
+
+// https://rebrand.ly/xharktank-pitches-http/?author=souptiksarkar4572&url=xharktank.herokuapp.com
+
+// https://rebrand.ly/xharktank-invest-http/?author=souptiksarkar4572&url=<Your HTTP backend url>
+
